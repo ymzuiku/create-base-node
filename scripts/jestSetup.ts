bd28de0d5f2fb878ae66e8e624cc3b7fa781aa78
pillar <ymblender@gmail.com>
2021-12-22 22:14:22 +0800

    fix: remove scripts/jest.setup

--- scripts/jestSetup.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-// Defind some thing before all test
-import { config } from "dotenv";
-import "./proxyFetch";
-
-(window as any).isTest = true;
-config();
